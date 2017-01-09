56dca0883770afca60ea7c0131df012208572fb6
Michael Aubrey <mcaubrey511@gmail.com>
2017-01-09 07:31:05 +0000

    fixed redirect logic

--- js/redirect.js
+++ js/redirect.js
@@ -1,10 +1,11 @@
 /* global getCookie */
 
 var cookieLanguage = getCookie("lang");
-var pageLanguage = window.location.href.indexOf("jisho.com/ja") !== -1 ? "en" : "";
-var pageLanguage = window.location.href.indexOf("jisho.com/en") !== -1 ? "ja" : "";
+var pageLanguage = window.location.href.indexOf("jisho.com/ja") !== -1 ? "en" : "ja";
+var pageLanguage = window.location.href.indexOf("jisho.com/en") !== -1 &&
+    window.location.href.indexOf("jisho.com/ha") !== -1 ? "" : pageLanguage;
 var redirectDestination = cookieLanguage === "" ? pageLanguage : cookieLanguage;
 
 if (redirectDestination !== "") {
-    window.location.replace("http://www.pokejisho.com/"+redirectDestination+"/jisho");
+    window.location.replace("http://www.pokejisho.com/" + redirectDestination + "/jisho");
 }
