69ea5acd49c331ca3c40054b36d2b1efec38a30c
Michael Aubrey <mcaubrey511@gmail.com>
2017-01-09 08:19:56 +0000

    added domain to cookies

--- js/cookies.js
+++ js/cookies.js
@@ -15,5 +15,5 @@ function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + "; " + expires + ";domain='pokejisho.com'";
+    document.cookie = cname + "=" + cvalue + "; " + expires + ";domain=pokejisho.com;path=/";
 }
