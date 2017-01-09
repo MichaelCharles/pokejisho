0bbcd33013c4b0cc2a6c5c0884284813792299c0
Michael Aubrey <mcaubrey511@gmail.com>
2017-01-09 08:37:41 +0000

    prioritize page language

--- js/redirect.js
+++ js/redirect.js
@@ -5,7 +5,7 @@ var cookieLanguage = getCookie("language");
 var pageLanguage = window.location.href.indexOf("jisho.com/ja") !== -1 ? "ja" : "en";
 var pageLanguage = window.location.href.indexOf("jisho.com/en") === -1 &&
     window.location.href.indexOf("jisho.com/ja") === -1 ? "" : pageLanguage;
-var redirectDestination = cookieLanguage === "" ? pageLanguage : cookieLanguage;
+var redirectDestination = pageLanguage === "" ? cookieLanguage : pageLanguage;
 
 if (redirectDestination !== "") {
     window.location.replace("http://www.pokejisho.com/" + redirectDestination + "/jisho");
