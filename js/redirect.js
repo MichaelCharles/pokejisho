7be1518e30502686ef108717ef1b59b0fdbb21b6
Michael Aubrey <mcaubrey511@gmail.com>
2017-01-09 07:33:28 +0000

    trying to fix redirect logic

--- js/redirect.js
+++ js/redirect.js
@@ -3,7 +3,7 @@
 var cookieLanguage = getCookie("lang");
 var pageLanguage = window.location.href.indexOf("jisho.com/ja") !== -1 ? "en" : "ja";
 var pageLanguage = window.location.href.indexOf("jisho.com/en") !== -1 &&
-    window.location.href.indexOf("jisho.com/ha") !== -1 ? "" : pageLanguage;
+    window.location.href.indexOf("jisho.com/ha") !== -1 ? pageLanguage : "";
 var redirectDestination = cookieLanguage === "" ? pageLanguage : cookieLanguage;
 
 if (redirectDestination !== "") {
