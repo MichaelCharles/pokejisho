07004ac3bfacc727e318ac78879386d57e041979
Michael Aubrey <mcaubrey511@gmail.com>
2017-01-09 07:44:15 +0000

    is it fixed now?

--- js/redirect.js
+++ js/redirect.js
@@ -1,9 +1,10 @@
 /* global getCookie */
 
-var cookieLanguage = getCookie("lang");
-var pageLanguage = window.location.href.indexOf("jisho.com/ja") !== -1 ? "en" : "ja";
-var pageLanguage = window.location.href.indexOf("jisho.com/en") !== -1 &&
-    window.location.href.indexOf("jisho.com/ha") !== -1 ? pageLanguage : "";
+var cookieLanguage = getCookie("pang");
+alert (cookieLanguage);
+var pageLanguage = window.location.href.indexOf("jisho.com/ja") !== -1 ? "ja" : "en";
+var pageLanguage = window.location.href.indexOf("jisho.com/en") === -1 &&
+    window.location.href.indexOf("jisho.com/ja") === -1 ? pageLanguage : "";
 var redirectDestination = cookieLanguage === "" ? pageLanguage : cookieLanguage;
 
 if (redirectDestination !== "") {
